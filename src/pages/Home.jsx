d9784a757c1a79225baf20104367b63d0489e95b
Rabiyacesen <154918374+Rabiyacesen@users.noreply.github.com>
2024-02-25 20:38:34 +0300

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <p>Kablosuz güvenli internet erişimi bulunan noktalarda, misafir internet hizmetine bağlanıldığında kullanıcı; internete giriş şifresi için, cep telefonunun mesaj bölümüne internet bir boşluk bırakıp, 583 bir boşluk ve giriş için belirlediği şifreyi yazıp, 3172´ ye SMS ile gönderecek. (Ömek: frıternet 583 abc 123) Birkaç dakika içinde hesap açılacak. Kullanıcı adı olarak cep telefonu numarası başında sıfır kullanmadan (5XX XXXXXXX) yazılacak ve şifre olarak da mesajda belirlenen şifre yazılarak güvenli internet girişi sağlanmış olacak.</p>
+
+
+
+    </div>
+  )
+}
+
+export default Home
